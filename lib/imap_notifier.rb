738131ad578f261be507b9f5240b2e38238cf34e
Kepi <kepi@igloonet.cz>
2014-03-22 20:09:52 +0100

    Default log shouldn't be to Desktop which may be absent on linux

--- lib/imap_notifier.rb
+++ lib/imap_notifier.rb
@@ -9,7 +9,7 @@ require 'imap_notifier/base'
 MAX_MAIL    = 5 # Max new mails mentioned before they
                 # are grouped together
 SLEEP       = 30
-ERRFILE     = File.expand_path('~/Desktop/imap_notifier.log')
+ERRFILE     = File.expand_path('/tmp/imap_notifier.log')
 DEBUGFILE   = '/tmp/imap_notifier.debug'
 PIDFILE     = '/tmp/imap_notifier.pid'
 IMAP_SERVER = "imap.gmail.com" # could be anything with tweaks
